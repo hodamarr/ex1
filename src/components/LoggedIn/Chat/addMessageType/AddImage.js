45f079c04606c40091c3013de5122e2e744e0f9c
ofeka <ofeka@LAPTOP-F7UD0GCF>
2022-04-25 09:50:37 +0300

    Messages types

---
+++ src/components/LoggedIn/Chat/addMessageType/AddImage.js
@@ -0,0 +1,36 @@
+import { useState, useRef } from "react";
+
+function AddImage() {
+    const [pictureInChat, SetPictureInChat] = useState();
+    const userRef = useRef();
+
+    function imageMessage(pictureInChat) {
+
+    }
+    return (
+        <div className="modal fade" id="ImagePick" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div className="modal-dialog">
+                <div className="modal-content">
+                    <div className="modal-header">
+                        <h5 className="modal-title" id="exampleModalLabel">Uploade Image</h5>
+                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div className="modal-body">
+                        <form>
+                            <div className="mb-3">
+                                <input className="form-control" accept="image/*" type="file" id="imageFile" ref={userRef}
+                                    onChange={(e) => { SetPictureInChat(URL.createObjectURL(e.target.files[0])); }}
+                                ></input>
+                            </div>
+                        </form>
+                    </div>
+                    <div className="modal-footer">
+                        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                        <button type="button" className="btn btn-primary" onClick={imageMessage()} data-bs-dismiss="modal">Done</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default AddImage;
