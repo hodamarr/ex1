b337ba7cd0263d226536572c2e697784791c51cd
ofeka <ofeka@LAPTOP-F7UD0GCF>
2022-04-25 10:14:45 +0300

    contacts Names

--- src/components/LoggedIn/contacts/contactItem/ContactItem.js
+++ src/components/LoggedIn/contacts/contactItem/ContactItem.js
@@ -1,14 +1,14 @@
 
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
-function ContactItem(pic){
-    return(
+function ContactItem(props) {
+    return (
         <div  >
-        <span className="navbar-text">
-                    <img src={pic} width="40" height="40" ></img>
-                    <a aria-current="page" style={{padding:"10px"}}>Active</a>
-                    <a style={{float:"right", color:"gray"}}>22:02</a>
-                    </span>
+            <span className="navbar-text">
+                <img src={props.pic} width="40" height="40" ></img>
+                <a aria-current="page" style={{ padding: "10px" }}>{props.name}</a>
+                <a style={{ float: "right", color: "gray" }}>{props.time}</a>
+            </span>
         </div>
     );
 }
